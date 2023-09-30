bd978071cce20fa399d0c9e1e8e523d1d26c7eae
Sulwe <cypriansulwe.cs@gmail.com>
2023-09-30 23:21:36 +0300

    add function to play the game
     create a function that compares the computer and player choices
     and returns the winner. Keeps track of the
     rounds and exits after round five.

--- script.js
+++ script.js
@@ -1,9 +1,51 @@
+let rounds = 0;
+
+function playGame() {
+
 function getComputerChoice() {
     const arr = ['Rock', 'Paper', 'Scissors'];
-    const randomIndex = Math.floor(Math.random() * arr.length);
+    let randomIndex = Math.floor(Math.random() * arr.length);
 
-    const computerChoice = arr[randomIndex];
+    let computerChoice = arr[randomIndex];
     return computerChoice;
+}
+let computerSelection = getComputerChoice();
+
+let playerSelection = prompt('Please input your choice');
+console.log(playerSelection);
+console.log(computerSelection);
+
+function playRound(){
+    
+    if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'paper'){
+        console.log('You won! Paper beats Rock!');
+    }   else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'rock'){
+        console.log('Computer won! Paper beats Rock!');
+    }   else if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'scissors'){
+        console.log('Computer won! Rock beats Scissors!');
+    }   else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'rock'){
+        console.log('You won! Rock beats Scissors!');
+    }   else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'scissors'){
+        console.log('You won! Scissors beats Paper!');
+    }   else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'paper'){
+        console.log('Computer won! Scissors beats Paper!');
+    }   else {
+            console.log('Invalid choice, please input Rock, Paper or Scissors')
+    }
+}
+
+playRound();
+
+rounds++;
+
+console.log(`Round number ${rounds}`);
+
+}
+
+
+
+while(rounds < 5) {
+    playGame();
 }
 
 
