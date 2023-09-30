fa0d0fc1faf3ea3ef154c41095a7dfb3f5d7a838
Sulwe <cypriansulwe.cs@gmail.com>
2023-09-30 21:25:54 +0300

    add a function to generate computer choice

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 function getComputerChoice() {
     const arr = ['Rock', 'Paper', 'Scissors'];
     const randomIndex = Math.floor(Math.random() * arr.length);
-    
+
+    const computerChoice = arr[randomIndex];
+    return computerChoice;
 }
 
-getComputerChoice();
+
