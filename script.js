8a0d998c3894a3ef411486833e0201b2081950b1
Sulwe <cypriansulwe.cs@gmail.com>
2023-11-30 23:13:04 +0300

    change console logs to DOM methods
    create a div and populate the game status as node children

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let rounds = 0;
 
 let computerWins = 0;
 let playerWins = 0;
+let winner = " ";
 
 function playGame() {
 
@@ -26,28 +27,36 @@ function playRound(){
     
     if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'paper'){
         console.log('You won! Paper beats Rock!');
+        winner = 'You won! Paper beats Rock!';
         playerWins++;
     }   else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'rock'){
         console.log('Computer won! Paper beats Rock!');
+        winner = 'Computer won! Paper beats Rock!'
         computerWins++;
     }   else if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'scissors'){
         console.log('Computer won! Rock beats Scissors!');
+        winner = 'Computer won! Rock beats Scissors!'
         computerWins++;
     }   else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'rock'){
         console.log('You won! Rock beats Scissors!');
+        winner = 'You won! Rock beats Scissors!'
         playerWins++;
     }   else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'scissors'){
         console.log('You won! Scissors beats Paper!');
+        winner = 'You won! Scissors beats Paper!'
         playerWins++;
     }   else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'paper'){
         console.log('Computer won! Scissors beats Paper!');
+        winner = 'Computer won! Scissors beats Paper!'
         computerWins++;
     }   else if (computerSelection.toLowerCase() === playerSelection.toLowerCase()){
         console.log('You both selected the same entry, please try again!');
+        winner = " ";
         playerWins = playerWins;
         computerWins = computerWins;
     }   else {
             console.log('Invalid choice, please input Rock, Paper or Scissors');
+            winner = " ";
             playerWins = playerWins;
             computerWins = computerWins;
     }
@@ -60,6 +69,21 @@ rounds++;
 console.log(`Round number ${rounds}`);
 console.log(`Computer Points = ${computerWins}`);
 console.log(`Player Points = ${playerWins}`);
+
+
+const div = document.querySelector("body");
+
+const declaration = document.createElement("p");
+declaration.textContent = winner;
+div.appendChild(declaration);
+
+const compPoints = document.createElement("p");
+compPoints.textContent = `Computer Points = ${computerWins}`;
+div.appendChild(compPoints);
+
+const playerPoints = document.createElement("p");
+playerPoints.textContent = `Player Points = ${playerWins}`;
+div.appendChild(playerPoints);
 
 }
 
@@ -75,6 +99,9 @@ if (playerWins > computerWins) {
 }  else {
     console.log('The game is a draw!');
 }
+
+
+
 
 
 
